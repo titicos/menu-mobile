83851ab4234f5b79f9e226987b433ccf410c4e07
TIAGO DA SILVA SOUZA <admtiagosouza@gmail.com>
2022-09-05 20:51:01 -0400

    subindo menu mobile

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let menu = document.querySelector(".menu");
+let menuMobile = document.querySelector(".menu-mobile")
+
+menu.addEventListener("click", function(){
+
+    menuMobile.classList.toggle("aparecer");
+})
